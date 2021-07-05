d781d8fcc0824826b2421b5b005025df6efd30c0
jinyaoMa <jinyao.ma@outlook.com>
2021-07-04 23:04:48 -0400

    .

---
+++ view/src/components/CheckSquare.vue
@@ -0,0 +1,40 @@
+<template>
+  <i
+    class="check-square far"
+    :class="{
+      'fa-check-square': this.isChecked,
+      'fa-square': !this.isChecked,
+    }"
+    @click="handleClick"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    checked: {
+      type: Boolean,
+      default() {
+        return false;
+      },
+    },
+  },
+  data() {
+    return {
+      isChecked: this.checked,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.isChecked = !this.isChecked;
+      this.$emit("click", this.isChecked);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.check-square {
+  font-size: 1em;
+}
+</style>
--- view/src/views/Home.vue
+++ view/src/views/Home.vue
@@ -7,8 +7,7 @@
           <thead>
             <tr>
               <td class="col-select">
-                <CheckBox />
-                <input type="checkbox" v-model="isSelectAll" />
+                <CheckSquare />
               </td>
               <td>{{ $locale.home.filename }}</td>
               <td class="col-size">{{ $locale.home.size }}</td>
@@ -17,10 +16,24 @@
           </thead>
           <tbody>
             <tr v-for="file in $files" :key="file.ID">
-              <td>
-                <input type="checkbox" v-model="file.isSelected" />
+              <td class="col-select">
+                <CheckSquare :checked="file.isSelected" />
               </td>
-              <td>{{ file.Path }}</td>
+              <td>
+                <div class="col-filename">
+                  <div class="col-filename-start">
+                    <i
+                      class="fas fa-fw"
+                      :class="{
+                        'fa-folder': file.Type === 'directory',
+                        'fa-file': file.Type === 'file',
+                      }"
+                    />
+                    {{ file.Path }}
+                  </div>
+                  <div class="col-filename-end"></div>
+                </div>
+              </td>
               <td>{{ $convertSpace2String(file.Size) }}</td>
               <td>{{ $formatDate(file.UpdatedAt, "YYYY-MM-DD HH:mm") }}</td>
             </tr>
@@ -34,13 +47,13 @@
 <script>
 import Layout from "../components/Layout.vue";
 import TaskBar from "../components/TaskBar.vue";
-import CheckBox from "../components/CheckBox.vue";
+import CheckSquare from "../components/CheckSquare.vue";
 
 export default {
   components: {
     Layout,
     TaskBar,
-    CheckBox,
+    CheckSquare,
   },
   data() {
     return {
@@ -84,12 +97,40 @@ export default {
 table {
   border-collapse: collapse;
   width: 100%;
+  table-layout: fixed;
+  margin-top: 1em;
   td {
     font-size: 0.9em;
     line-height: 3.2;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  tr {
+    transition: 0.2s;
+    &:hover {
+      background-color: #fafbfc;
+    }
   }
   .col-select {
     width: 2em;
+    padding-left: 1em;
+  }
+  .col-filename {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .col-filename-start {
+    flex-grow: 1;
+    > i {
+      margin-right: 0.5em;
+      transform: scale(1.3);
+      transform-origin: center center;
+    }
+  }
+  .col-filename-end {
+    margin-right: 1em;
   }
   .col-size {
     width: 100px;
